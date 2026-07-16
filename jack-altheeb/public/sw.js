6aa3df814ed8ccfb806d5035d2669b8c9d472cae
Q8-ux <bo.mash3aaan@gmail.com>
2026-07-16 21:15:49 +0300

    Disable the stale service worker cache for V3

--- jack-altheeb/public/sw.js
+++ jack-altheeb/public/sw.js
@@ -1,36 +1,13 @@
-const CACHE = "jack-altheeb-v5";
-const ASSETS = [
-  "/",
-  "/style.css?v=20260716-5",
-  "/enhancements.css?v=20260716-5",
-  "/app.js?v=20260716-5",
-  "/i18n.js?v=20260716-5",
-  "/manifest.webmanifest?v=5",
-  "/assets/art.svg"
-];
+const CACHE_VERSION = "jack-altheeb-disabled-v7";
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
+self.addEventListener("install", () => self.skipWaiting());
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.url.includes("/socket.io/")) return;
-  event.respondWith(
-    fetch(event.request, { cache: "no-store" })
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: "window" }))
+      .then(clients => clients.forEach(client => client.navigate(client.url)))
   );
 });
