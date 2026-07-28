59e68eb9176a73a7c592176aecf68147b598e1a5
Q8-ux <bo.mash3aaan@gmail.com>
2026-07-28 11:01:29 +0300

    Add attendance module

---
+++ workforce-platform/apps/api/src/attendance/attendance.module.ts
@@ -0,0 +1,3 @@
+import { Module } from '@nestjs/common';
+import { AttendanceController } from './attendance.controller';
+@Module({controllers:[AttendanceController]}) export class AttendanceModule {}
