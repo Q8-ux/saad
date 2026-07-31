0d20af44bc4b32a0dfb5a9a1f25d966ec2f5da87
Q8-ux <bo.mash3aaan@gmail.com>
2026-07-31 21:11:29 +0300

    feat(workforce): add emergency management module

---
+++ workforce-platform/apps/api/src/emergency/emergency.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { EmergencyController } from './emergency.controller';
+
+@Module({ controllers: [EmergencyController] })
+export class EmergencyModule {}
