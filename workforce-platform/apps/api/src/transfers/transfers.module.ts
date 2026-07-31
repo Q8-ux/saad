ae877dc2e461da19f1328920425d6b119af1980f
Q8-ux <bo.mash3aaan@gmail.com>
2026-07-31 20:49:43 +0300

    feat(workforce): add transfer permits module

---
+++ workforce-platform/apps/api/src/transfers/transfers.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { TransfersController } from './transfers.controller';
+
+@Module({ controllers: [TransfersController] })
+export class TransfersModule {}
